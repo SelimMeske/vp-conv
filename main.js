ceb7a4e5841f9a0d609b0bfb32554970ee1717d6
Selim Mesic <selim.mesic@virginpulse.com>
2021-11-22 19:59:46 +0100

    Made live update

--- main.js
+++ main.js
@@ -4,12 +4,51 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var inputField = document.querySelector('#input');
     var outputFiled = document.querySelector('#output');
 
-    convertButton.addEventListener('click', function(){
+    inputField.addEventListener('input', e => {
+        mainConverter()
+    });
+
+    function mainConverter() {
+        var stringTagStart = []
+        
         var inputFieldContent = inputField.value;
+        
+
+        const regex = /<string name=".*".*?>/gm;
+        
+        let m;
+
+        while ((m = regex.exec(inputFieldContent)) !== null) {
+            // This is necessary to avoid infinite loops with zero-width matches
+            if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+            
+            // The result can be accessed through the `m`-variable.
+            m.forEach((match, groupIndex) => {
+                stringTagStart.push(match); 
+            });
+        }
+
+        stringTagStart.forEach((obje, index) => {
+            var scriptOpenTag = `[trlOpenScript=${index}]`;
+            var scriptCloseTag = `[trlCloseScript]`;
+            inputFieldContent = inputFieldContent.replace(obje, scriptOpenTag);
+            inputFieldContent = inputFieldContent.replace('</string>', scriptCloseTag)
+        });
+
+        // Parse HTML Chars
         var parsedLT = inputFieldContent.replace(/</g, "&lt;");
         var parsedGT = parsedLT.replace(/>/g, "&gt;");
 
+        stringTagStart.forEach((obje, index) => {
+            var scriptOpenTag = `[trlOpenScript=${index}]`;
+            var scriptCloseTag = `[trlCloseScript]`;
+            parsedGT = parsedGT.replace(scriptOpenTag, obje);
+            parsedGT = parsedGT.replace(scriptCloseTag, '</string>')
+        });
+
         outputFiled.value = parsedGT;
-    });
+    }
 
 });
